170a5a24d665d5ab203cb57e9f0d55efde4d9804
noppoman <yuki@miketokyo.com>
2016-05-11 14:17:23 +0900

    update dependencies

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "WS",
     dependencies: [
-        .Package(url: "https://github.com/slimane-swift/Hanger.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/noppoMan/Crypto.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/slimane-swift/Hanger.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/noppoMan/Crypto.git", majorVersion: 0, minor: 3)
     ]
 )
