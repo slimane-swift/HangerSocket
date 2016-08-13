46306bd497cd9578676b6fd4ba0ff0890c3ec335
noppoman <yuki@miketokyo.com>
2016-08-13 11:10:55 +0900

    migrating to slimanw-swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         .Package(url: "https://github.com/Zewo/Event.git",  majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/Zewo/POSIX.git", majorVersion: 0, minor: 12),
         .Package(url: "https://github.com/noppoMan/Suv.git", majorVersion: 0, minor: 10),
-        .Package(url: "https://github.com/noppoMan/AsyncHTTPSerializer.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/slimane-swift/AsyncHTTPSerializer.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/noppoMan/Crypto.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/slimane-swift/HTTP.git", majorVersion: 0, minor: 12),
         .Package(url: "https://github.com/slimane-swift/HTTPParser.git", majorVersion: 0, minor: 12),
